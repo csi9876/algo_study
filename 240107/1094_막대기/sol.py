011b8687433e5ab6d379a1f685785170d7267ed3
csi <qjxltm@naver.com>
2024-01-07 21:10:28 +0900

    240107

---
+++ 240107/1094_막대기/sol.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt')
+
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+count = 0
+n = 64
+
+while x > 0:
+	if n > x:
+		n = n // 2
+	else:
+		count += 1
+		x -= n
+
+print(count)
