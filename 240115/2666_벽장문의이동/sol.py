b9154f085bde7fc23d4eba72afcae1e40de8e92d
csi <qjxltm@naver.com>
2024-02-08 16:33:00 +0900

    240115 fix

--- 240115/2666_벽장문의이동/sol.py
+++ 240115/2666_벽장문의이동/sol.py
@@ -1,13 +1,32 @@
 import sys
 sys.stdin = open('input.txt')
 
+
 import sys
 input = sys.stdin.readline
 
-
 n = int(input())
-s1, s2 = map(int, input().split())
-m = int(input())
-arr = list(int(input()) for _ in range(m))
+arr = list(map(int, input().split()))
+nums = [0] * 10
 
+def two_point():
+    l, r, cnt, kind, max_n = 0, 0, 0, 0, 0
+    while r < n:
+        if nums[arr[r]] == 0:
+            kind += 1
 
+        cnt += 1
+        nums[arr[r]] += 1
+
+        while kind > 2:
+            nums[arr[l]] -= 1
+            if nums[arr[l]] == 0:
+                kind -= 1
+            cnt -= 1
+            l += 1
+
+        max_n = max(max_n, cnt)
+        r += 1
+    return max_n
+
+print(two_point())
