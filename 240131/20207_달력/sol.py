d4ca3a4fe71b2e876388bf1694b6beaa7c3aaed7
csi <qjxltm@naver.com>
2024-03-25 17:32:55 +0900

    240131

--- 240131/20207_달력/sol.py
+++ 240131/20207_달력/sol.py
@@ -5,19 +5,19 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [0] * 365     # 달력 배열
+arr = [0] * 366     # 달력 배열
 
 for i in range(n):
     s, e = map(int, input().split())
     while s <= e:   # 달력 기간 체크
-        arr[s] += 1 # 일정 추가
+        arr[s] += 1     # 일정 추가
         s += 1
 
 res = 0
 x = 0   # 연속된 일정이 있는 일수 저장
 y = 0   # 가장 많이 겹친 일정 개수 저장
 
-for i in range(365):
+for i in range(366):
     if arr[i] == 0:     # 일정이 없으면
         res += x * y    # 연속 일정 * 가장 많이 겹친 일정
         x, y = 0, 0     # 초기화
